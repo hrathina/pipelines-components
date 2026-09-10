477e5efa4125688157a0a801a0194a5031afa679
Hari Haran Rathinakumar <hrathina@redhat.com>
2026-09-10 13:24:53 +0530

    build: use speculator 3.6 ea2 image

--- components/training/speculator/extraction/component.py
+++ components/training/speculator/extraction/component.py
@@ -9,7 +9,7 @@ _SHARED_DIR = os.path.join(os.path.dirname(__file__), "..", "shared")
 
 
 @dsl.component(
-    base_image="quay.io/opendatahub/odh-th-torch-cuda-py312@sha256:9ad2d72ebe892dffd3554eeb81e2a47c9fcf0d97f89ed261d48f4e8bbc367b4b",
+    base_image="quay.io/opendatahub/odh-th-torch-cuda-py312:3.6-ea.2",
     packages_to_install=["kfp==2.17.0", "kubernetes", "olot"],
     install_kfp_package=False,
     embedded_artifact_path=_SHARED_DIR,
