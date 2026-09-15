1683be4f7c3254ed4b2ec221fc13a1d573838062
Hari Haran Rathinakumar <hrathina@redhat.com>
2026-09-15 18:37:40 +0530

    Revert "feat: add speculator evaluation pipeline"
    This reverts commit f0afee79789f0c56152e6bba0168aa5e399f3da1.

--- components/evaluation/speculator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Speculative decoding evaluation component."""
-
-from .component import evaluate_speculator
-
-__all__ = ["evaluate_speculator"]
--- pipelines/training/speculator/train_only/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Speculator draft-model training pipeline."""
