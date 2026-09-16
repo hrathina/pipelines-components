b28fc6acb7a3b3dd3579aa72ba5dcd6ee47a2d2d
Hari Haran Rathinakumar <hrathina@redhat.com>
2026-09-16 09:53:52 +0530

    fix: pin evaluation image tag and digest

--- components/evaluation/speculator/component.py
+++ components/evaluation/speculator/component.py
@@ -5,7 +5,7 @@ from kfp import dsl
 
 @dsl.component(
     base_image=(
-        "quay.io/opendatahub/odh-th-torch-cuda-py312@"
+        "quay.io/opendatahub/odh-th-torch-cuda-py312:odh-3.6-ea.1@"
         "sha256:9ad2d72ebe892dffd3554eeb81e2a47c9fcf0d97f89ed261d48f4e8bbc367b4b"
     ),
     packages_to_install=["kfp==2.17.0", "requests>=2.32.0"],
